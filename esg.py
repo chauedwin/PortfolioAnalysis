e1c5ff6b3614c3616ce76a8d75d19f9dd56186e4
Edwin Chau <edwinchau117@gmail.com>
2021-08-26 17:20:54 -0700

    add benchmark and esg files

---
+++ esg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+
+sp = pd.read_csv("constituents_csv.csv")['Symbol'].tolist()
+nasdaq = pd.read_csv("nasdaq_screener.csv")['Symbol'].tolist()
+ticks = sp + nasdaq
+
+import re
+string = ''
+with open('rawinfo.txt') as f:
+    for i in range(1525):
+        string = string + f.readline()
+
+        parsed = re.findall('[A-Z^\t*]+', string)
+esgticks = [strings for strings in parsed if strings in ticks]
+esgticks = list(set(esgticks))
+
+from pipeline import Portfolio
+
+p = Portfolio(tickers = esgticks, start = '2016-07-01', end = '2021-07-01')
+p.download()
+p.clean_data(minmonths = 36)
+p.compute_weights()
+
+print(p.weights)
+
